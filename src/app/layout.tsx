ab2799ae88be434a13e1d436a749413296ae18f2
sunjie <sunjie1993sg@gmail.com>
2026-03-24 18:30:17 +0800

    update UI code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import {Montserrat} from "next/font/google";
 import {Providers} from "@/components/providers";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
+const montserrat = Montserrat({
+    variable: "--font-montserrat",
     subsets: ["latin"],
 });
 
@@ -19,15 +14,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${montserrat.variable} font-[family-name:var(--font-montserrat)] antialiased`}>
         <Providers>{children}</Providers>
         </body>
         </html>
